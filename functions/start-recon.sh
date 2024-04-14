456857d4475676affd2b2ebab6f796118b96f27e
G3kSec <lucianogriffa@protonmail.com>
2024-04-14 17:48:58 -0400

    feat: Start RECON

--- functions/start-recon.sh
+++ functions/start-recon.sh
@@ -4,22 +4,20 @@
 endColour="\033[0m\e[0m"
 greenColour="\e[0;32m\033[1m"
 redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
 yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
 whiteColour="\e[0;37m\033[1m"
 
 function startRecon() {
   tput civis
-  
   platformName="$1"
   companyName="$2"
-  
-  dir_path="./${platformName}/${companyName}"
-  
+  targets="$3"
+  platformSyntax=$(echo $platformName | sed 's/ //g')
+  companySyntax=$(echo $companyName | sed 's/ //g')
+  dir_path="./${platformSyntax}/${companySyntax}"
   echo -e "\t${greenColour}Bug Bounty Platform: ${endColour}${whiteColour}$platformName${endColour}"
-  echo -e "\t${greenColour}Target Company: ${endColour}${whiteColour}$companyName${endColour}"
+  echo -e "\t${greenColour}Company Name: ${endColour}${whiteColour}$companyName${endColour}"
+  echo -e "\t${greenColour}Targets: ${endColour}${whiteColour}$(echo $targets | sed 's/ /, /g')${endColour}"
   
   confirm=""
   question= echo -e "\n\t${whiteColour}Are these data correct? Enter${endColour} ${greenColour}(y)${endColour}${whiteColour} to continue or ${endColour}${greenColour}(n)${endColour}${whiteColour} to cancel${endColour}"
@@ -33,16 +31,20 @@ function startRecon() {
     tput cnorm && exit 1
   elif [ "$confirm" = "y" ]; then
     echo -e "\n\t${greenColour}The data is correct...Starting...${endColour}"
-    echo -e "\n\t${yellowColour}Creating folders and files...${endColour}"
     if [ ! -d "$dir_path" ]; then
+      echo -e "\n\t${yellowColour}Creating folders and files...${endColour}"
       mkdir -p "$dir_path"
       echo "Company Name: ${companyName}" >> "$dir_path/general-info.txt"
-      echo -e "Bug Program: https://hackerone.com/$companyName\n" >> "$dir_path/general-info.txt"
-      echo -e "Acquisitions and Company History:" >> "$dir_path/general-info.txt"
-      echo "https://en.wikipedia.org/wiki/$companyName" >> "$dir_path/general-info.txt"
-      echo "https://www.crunchbase.com/organization/$companyName" >> "$dir_path/general-info.txt"
-      echo "https://www.google.com/search?q=$companyName" >> "$dir_path/general-info.txt"
-      bat "$dir_path/general-info.txt"
+      echo "Bug Program: ${platformName}" >> "$dir_path/general-info.txt"
+      echo "Link Bug Program: https://hackerone.com/$(echo $companySyntax | tr '[:upper:]' '[:lower:]')" >> "$dir_path/general-info.txt"
+      echo "Description:" >> "$dir_path/general-info.txt"
+      echo -e "\nAcquisitions and Company History:" >> "$dir_path/general-info.txt"
+      echo "https://www.crunchbase.com/organization/$(echo $companySyntax | tr '[:upper:]' '[:lower:]')" >> "$dir_path/general-info.txt"
+      echo "https://en.wikipedia.org/wiki/$(echo $companySyntax | tr '[:upper:]' '[:lower:]')" >> "$dir_path/general-info.txt"
+      echo "https://www.google.com/search?q=$(echo $companySyntax | tr '[:upper:]' '[:lower:]')" >> "$dir_path/general-info.txt"
+      echo -e "\nTargets:" >> "$dir_path/general-info.txt"
+      echo "$(echo $targets | sed -r 's/ /\n/g')" >> "$dir_path/general-info.txt"
+      bat -A "$dir_path/general-info.txt"
       tput cnorm
     else
       echo -e "\n\t${yellowColour}The directory already exists.${endColour}\n"
